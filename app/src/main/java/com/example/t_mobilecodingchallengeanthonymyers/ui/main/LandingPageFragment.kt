833d3d1728f8e18d4b4e57a20ce5c4c6a5d60623
Anthony Myers <anthonym@ultra.me>
2021-05-25 16:19:48 -0400

    Restructured the adapter to work with multiple layouts.

--- app/src/main/java/com/example/t_mobilecodingchallengeanthonymyers/ui/main/LandingPageFragment.kt
+++ app/src/main/java/com/example/t_mobilecodingchallengeanthonymyers/ui/main/LandingPageFragment.kt
@@ -1,63 +1,28 @@
 package com.example.t_mobilecodingchallengeanthonymyers.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
+import com.example.t_mobilecodingchallengeanthonymyers.R
 import com.example.t_mobilecodingchallengeanthonymyers.adapters.CardAdapter
-import com.example.t_mobilecodingchallengeanthonymyers.databinding.LandingPageFragmentBinding
+import com.example.t_mobilecodingchallengeanthonymyers.databinding.FragmentLandingPageBinding
+import com.example.t_mobilecodingchallengeanthonymyers.utils.Event
 
-class LandingPageFragment : Fragment() {
-
-    private var _binding: LandingPageFragmentBinding? = null
-    private val binding: LandingPageFragmentBinding get() = _binding!!
+class LandingPageFragment : Fragment(R.layout.fragment_landing_page) {
 
     private val cardViewModel: CardViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = LandingPageFragmentBinding.inflate(inflater)
-        return binding.root
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        with(binding) {
-
-            cardViewModel.event.observe(viewLifecycleOwner, Observer {
-                when (it) {
-                    is CardViewModel.Event.Loading -> {
-                        loaderPb.isVisible = it.isLoading
-                    }
-                    is CardViewModel.Event.Success -> {
-                        loaderPb.isVisible = it.isLoading
-                        binding.contentRv.adapter = CardAdapter(it.data)
-                    }
-                    is CardViewModel.Event.Error -> {
-                        loaderPb.isVisible = it.isLoading
-                        binding.couldNotDownloadTv.visibility = TextView.VISIBLE
-                    }
-
-                }
-            })
+        FragmentLandingPageBinding.bind(view).apply {
+            cardViewModel.event.observe(viewLifecycleOwner) { event ->
+                loaderPb.isVisible = event is Event.Loading
+                couldNotDownloadTv.isVisible = event is Event.Error
+                if (event is Event.Success) contentRv.adapter = CardAdapter(event.data)
+            }
         }
-    }
-
-    override fun onDestroy() {
-        _binding = null
-        super.onDestroy()
-    }
-
-    companion object {
-        private val TAG = LandingPageFragment::class.java.simpleName
     }
 }
